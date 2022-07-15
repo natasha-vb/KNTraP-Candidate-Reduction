8be74629728b4f5d4ef573914fb927a93a8ceb03
natasha-vb <100886666+natasha-vb@users.noreply.github.com>
2022-07-15 18:01:52 +1000

    Create SE_filtering.py

---
+++ SE_filtering.py
@@ -0,0 +1,13 @@
+# Based on run_sourceextractor.py code by Jielai Zhang
+
+import docopt, os
+import astropy.io.fits as fits
+import subprocess
+from astropy.io import ascii
+import pandas as pd
+import numpy as np
+import ntpath, shutil
+import time
+import string    
+import random
+import shutil
