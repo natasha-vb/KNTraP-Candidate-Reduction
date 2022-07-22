2c1efe313be1c9c1b5e3f9aca645d8eae558b3f3
natasha-vb <nvanbemmel@swin.edu.au>
2022-07-22 18:26:06 +1000

    Update run_sextractor.py

--- utils/run_sextractor.py
+++ utils/run_sextractor.py
@@ -67,7 +67,7 @@ def run_sextractor(fitsfiles,spreadmodel = True, catending = None,
                             f'-STARNNW_NAME {nnw_path} -PIXEL_SCALE 0  -MAG_ZEROPOINT 25.0 '\
                             f'-PSF_NAME {f_psf} -PSF_NMAX 1 -PATTERN_TYPE GAUSS-LAGUERRE '\
                             f'-SEEING_FWHM {fwhm} -DETECT_MINAREA {detect_minarea} -DETECT_THRESH {detect_thresh} '\
-                            f'-CHECKIMAGE_TYPE SEGMENTATION,APERTURES -CHECKIMAGE_NAME seg.fits,aper.fits \'
+                            f'-CHECKIMAGE_TYPE SEGMENTATION,APERTURES -CHECKIMAGE_NAME seg.fits,aper.fits '\
                             f'-PHOT_APERTURES 8 \'
                             f'{f}'
             
