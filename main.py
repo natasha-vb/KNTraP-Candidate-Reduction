c46a22947ecf97686a260a819a8d16258fbb9396
natasha-vb <nvanbemmel@swin.edu.au>
2022-08-05 16:05:38 +1000

    Update main.py

--- main.py
+++ main.py
@@ -175,16 +175,23 @@ if __name__ == "__main__":
         #                                         detect_thresh=detect_thresh, ccd=ccd, field=args.field,
         #                                         diff_im=False, verbose=args.verbose)
         
+        
         # Read in unforced diff light curve files pathnames 
-        difflc_files = glob.glob(f'../../web/web/sniff/{args.field}_tmpl/{ccd}/*/.unforced.difflc.txt')
+        difflc_files = glob.glob(f'../../web/web/sniff/{args.field}_tmpl/{ccd}/*/.unforced.difflc.txt')        
 
-        for f in difflc_files:
+        for ii, f in enumerate(difflc_files):
+            
+            if verbose:
+                print(f'DIFFERENCE LIGHT CURVE FILES, CCD {ccd}:')
+                print(diffc_files[ii])
 
             # Read in diff lc file as Pandas table
             df = read_file(f)
 
             det_dates = [df["dateobs"]]
-            print('DETECTION DATES: ', det_dates)
+
+            if verbose:
+                print('DETECTION DATES: ', det_dates)
 
         
 
