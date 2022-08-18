42e66fb1149cd92b3852c8f97a37467c44abfb6c
natasha-vb <nvanbemmel@swin.edu.au>
2022-08-18 17:13:36 +1000

    moving cat_match to separate script

--- main.py
+++ main.py
@@ -14,65 +14,7 @@ from astropy.coordinates import SkyCoord
 from astropy import units as u
 
 from utils import run_sextractor
-
-def cat_match(date, ra, dec, filt, field='257A', ccd='1'):
-        match_list = glob.glob(f'./cats/{field}/{ccd}/*.{date}.*_{filt}_*.cat')
-        df_cattmp = pd.DataFrame()
-
-        df_cattmp["dateobs"] = [f"{date}"]
-        df_cattmp["filt"]    = [f"{filt}"]
-
-        for m in match_list:
-            cat = ascii.read(m)
-            df_cat = pd.DataFrame(cat.as_array())
-
-            ps = re.compile("sci")
-            m_sci = ps.search(m)
-            pt = re.compile("tmpl")
-            m_tmpl = pt.search(m)
-            if m_sci:
-                column_ending = "SCI"
-            elif m_tmpl:
-                column_ending = "TMPL"
-            else:
-                column_ending = "DIFF"
-
-            # Matching detection coordinates to SE catalogue
-            coords_det = SkyCoord(ra=[ra], dec=[dec],unit='deg')
-            coords_cat = SkyCoord(ra=df_cat["X_WORLD"], dec=df_cat["Y_WORLD"],unit='deg')
-
-            idx, d2d, d3d = coords_det.match_to_catalog_3d(coords_cat)
-
-            # Separation constraint (~ 1 arcsec) ### might not match with only 1 arcsec, try 2 arcsecs
-            sep_constraint = d2d < (2 * u.arcsec)
-
-            df_cat_matched = df_cat.iloc[idx[sep_constraint]]
-
-            if df_cat_matched.empty:
-                # for col in df_cat_matched.columns:
-                #     if col == "NUMBER":
-                #         continue
-                #     df[col].values[:] = [" "]
-                
-                if verbose:
-                    print(f'NO DETECTION MATCH FOUND IN {column_ending} CATALOG:', m)
-
-            df_cat_matched = df_cat_matched.reset_index(drop=False)
-            
-            df_cattmp[f"MAG_AUTO_{column_ending}"]     = df_cat_matched["MAG_AUTO"]
-            df_cattmp[f"MAGERR_AUTO_{column_ending}"]  = df_cat_matched["MAGERR_AUTO"]
-            df_cattmp[f"X_WORLD_{column_ending}"]      = df_cat_matched["X_WORLD"]
-            df_cattmp[f"Y_WORLD_{column_ending}"]      = df_cat_matched["Y_WORLD"]
-            df_cattmp[f"X_IMAGE_{column_ending}"]      = df_cat_matched["X_IMAGE"]
-            df_cattmp[f"Y_IMAGE_{column_ending}"]      = df_cat_matched["Y_IMAGE"]
-            df_cattmp[f"CLASS_STAR_{column_ending}"]   = df_cat_matched["CLASS_STAR"]
-            df_cattmp[f"ELLIPTICITY_{column_ending}"]  = df_cat_matched["ELLIPTICITY"]
-            df_cattmp[f"FWHM_WORLD_{column_ending}"]   = df_cat_matched["FWHM_WORLD"]
-            df_cattmp[f"FWHM_IMAGE_{column_ending}"]   = df_cat_matched["FWHM_IMAGE"]
-            df_cattmp[f"SPREAD_MODEL_{column_ending}"] = df_cat_matched["SPREAD_MODEL"]
-            df_cattmp[f"FLAGS_{column_ending}"]        = df_cat_matched["FLAGS"]
-        
-        return df_cattmp
+from utils import cat_match
 
 def read_file(fname):
     try:
@@ -299,7 +241,7 @@ if __name__ == "__main__":
                 dec = df["dec"][ii]
                 filt = df["filt"][ii]
 
-                match_cat_table = cat_match(date, ra, dec, filt, field=args.field, ccd=ccd)
+                match_cat_table = cat_match(date, ra, dec, filt, field=args.field, ccd=ccd, verbose=args.verbose)
 
                 df_out = pd.merge(df, match_cat_table, how='left', on=['dateobs','filt'])  ### merge by columns dateobs, filt
 
