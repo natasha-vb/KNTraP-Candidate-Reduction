87bfed891133f90ba77625c34fe06547ed994417
natasha-vb <nvanbemmel@swin.edu.au>
2022-09-09 18:11:12 +1000

    Update find_good_detections.py

--- utils/misc/find_good_detections.py
+++ utils/misc/find_good_detections.py
@@ -1,3 +1,19 @@
 import numpy as np
 
-def find_good_detections():
+def find_good_detections(det_file):
+
+    # Checking ellipticity requirement (Ellip. < 0.7)
+    ell = det_file["ELLIPTICITY_DIFF"]
+
+
+    # Checking FWHM requirement (FWHM < )
+
+
+    # Checking Spread Model requirement ()
+
+
+    ##########################
+    # make copy of lc file
+    # delete rows of detections which do not meet criteria 
+    # find length of file after cuts
+    # return this value as number of good detections 
