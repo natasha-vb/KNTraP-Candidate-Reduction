d5d16127fd469db80d3a3ec67eae41fed4970f26
natasha-vb <nvanbemmel@swin.edu.au>
2022-10-04 14:58:39 +1100

    Update main.py

--- main.py
+++ main.py
@@ -251,7 +251,6 @@ if __name__ == "__main__":
                 det_dates = df["dateobs"].values 
                 for ii, d in enumerate(det_dates):
                     det_dates[ii] = d.replace("-", "")[2:8]
-                df["dateobs"] = float(det_dates)
                 df = df.sort_values(by="dateobs")
                 
                 # Converting ra and dec to degrees
@@ -268,6 +267,7 @@ if __name__ == "__main__":
                     print(df[["dateobs", "ra", "dec"]])
                     
                 for ii, d in enumerate(det_dates):
+                    df["dateobs"][ii] = float(df["dateobs"][ii])
                     date = df["dateobs"][ii]
                     ra = df["ra"][ii]
                     dec = df["dec"][ii]
