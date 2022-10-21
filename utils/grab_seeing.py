1f32adbdb48f3479f94a850c2f66c807c6a73498
natasha-vb <nvanbemmel@swin.edu.au>
2022-10-21 17:58:30 +1100

    Update grab_seeing.py

--- utils/grab_seeing.py
+++ utils/grab_seeing.py
@@ -3,9 +3,17 @@ import pandas as pd
 
 def grab_seeing(lcfile, field, ccd):
 
-    for d in lcfile['dateobs']:
-        date = (d + 20000000) - 1 
-        obslog = glob.glob(f'./observinglogs/{date}.qcinv')
+    df_seeing = pd.DataFrame[]
+
+    for i in range(len(lcfile)):
+        lc_row = lcfile.iloc[i]
+        date = lc_row['dateobs']
+        filter = lc_row['filt']
+
+        # Adjusting date to match format in qcinv filenames (YYYYMMDD)
+        date_adj = (d + 20000000) - 1 
+        obslog = glob.glob(f'./observinglogs/{date_adj}.qcinv')
+
         if date == 20220221:
             df_obslog = pd.read_csv(obslog,header=None, skiprows=[0,1,2,3], delim_whitespace=True)
         
@@ -14,6 +22,9 @@ def grab_seeing(lcfile, field, ccd):
         
         df_obslog.columns = ['#expid', 'ra', 'dec', 'ut', 'fil', 'time', 'secz', 'psf', 'sky', 'cloud', 'teff', 'Object']
 
-        df_obslog_field = df_obslog[df_obslog['Object'] = field]
+        df_obslog_field = df_obslog[df_obslog['Object'] == field]
 
+        df_obslog_seeing = df_obslog_field['psf'][df_obslog_field['filt'] == filter]
+
+        if df_obslog_seeing
         
