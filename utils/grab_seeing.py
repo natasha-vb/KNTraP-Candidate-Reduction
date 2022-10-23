67c2159986eb840c4ec8e792cdef8210334a3b00
natasha-vb <nvanbemmel@swin.edu.au>
2022-10-23 20:22:37 +1100

    Update grab_seeing.py

--- utils/grab_seeing.py
+++ utils/grab_seeing.py
@@ -14,17 +14,17 @@ def grab_seeing(lcfile, field, ccd):
         date_adj = (d + 20000000) - 1 
         obslog = glob.glob(f'./observinglogs/{date_adj}.qcinv')
 
+        # Skip the empty rows at top of the DataFrame
         if date == 20220221:
             df_obslog = pd.read_csv(obslog,header=None, skiprows=[0,1,2,3], delim_whitespace=True)
-        
         else:
             df_obslog = pd.read_csv(obslog,header=None, skiprows=[0,1,2], delim_whitespace=True)
         
         df_obslog.columns = ['#expid', 'ra', 'dec', 'ut', 'fil', 'time', 'secz', 'psf', 'sky', 'cloud', 'teff', 'Object']
 
         df_obslog_field = df_obslog[df_obslog['Object'] == field]
-
         df_obslog_seeing = df_obslog_field['psf'][df_obslog_field['filt'] == filter]
 
-        if df_obslog_seeing
-        
+        if df_obslog_seeing > 1:
+            # sum up all the seeing values and average them
+            
