159ef82a9469d06fef3d9c3180bb91e02d6912e7
natasha-vb <nvanbemmel@swin.edu.au>
2022-10-27 13:57:01 +1100

    Update top_candidates.py

--- top_candidates.py
+++ top_candidates.py
@@ -335,7 +335,7 @@ if __name__ == "__main__":
 
             else:
                 for m in mcut_list:
-                    m_df = pd.read_csv(m, sep=',', skiprows=[1:11])
+                    m_df = pd.read_csv(m, sep=',', skiprows=[1,2,3,4,5,6,7,8,9,10])
 
                     m_ccd = m_df[m_df['CCD'] == ccd]
                     m_ccd_len = len(m_ccd)
