80b02f3bc644b7998998bb726d9bcb57c22619e7
natasha-vb <nvanbemmel@swin.edu.au>
2022-12-13 14:32:59 +1100

    Update mag_rates.py

--- utils/mag_rates.py
+++ utils/mag_rates.py
@@ -1,10 +1,36 @@
-import glob
 import pandas as pd
+
+def calculate_mag_diff(lc):
+
+    alpha_temp = pd.DataFrame()
+
+    for i in range(len(lc)):
+        lc_row = lc.iloc[i]
+        date = lc_row['dateobs']
+        filter = lc_row['filt']
+        mag_2 = lc_row['m']
+
+        # Calculate change in mag from previous night, alpha
+        if i == 1:
+            alpha = 'NaN'
+        else:
+            lc_prev_row = lc.iloc[i-1]
+            mag_1 = lc_prev_row['m']
+            alpha = mag_2 - mag_1
+        
+        alpha_temp = alpha_temp.append([{'dateobs': date,
+                                         'filt':filter,
+                                         'alpha':alpha}])
+    return alpha_temp
 
 def mag_rates(lc_file):
 
-    # ITERATE THOUGH MAGS FOR I AND G BANDS
-    # CALCULATE ALPHA VALUE 
-    # SAVE INTO TEMP DATAFRAME TO APPEND BACK TO LC FILE
-    # REMEMBER TO LEAVE FIRST ROW BLANK
-    
+    lc_file_i = lc_file[lc_file['filt'] == 'i']
+    lc_file_g = lc_file[lc_file['filt'] == 'g']
+
+    alpha_i = calculate_mag_diff(lc_file_i)
+    alpha_g = calculate_mag_diff(lc_file_g)
+
+    alpha = pd.merge(alpha_i, alpha_g, how='left', on=['dateobs','filt'])
+
+    return alpha
