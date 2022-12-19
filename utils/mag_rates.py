f1cb7ad24e6fb20d5bb547bf8b414ac0869fcb75
natasha-vb <nvanbemmel@swin.edu.au>
2022-12-19 20:05:36 +1100

    Update mag_rates.py

--- utils/mag_rates.py
+++ utils/mag_rates.py
@@ -5,35 +5,48 @@ def calculate_mag_diff(lc, verbose=False):
 
     alpha_temp = pd.DataFrame()
 
-    for i in range(len(lc)):
-       
-        lc_row = lc.iloc[i]
-        date_2 = lc_row['dateobs']
-        filter = lc_row['filt']
-        mag_2 = lc_row['m']
+    lc  = lc['m'].replace('-', np.NaN)
 
-        # Calculate change in mag from previous detection, alpha (mag/night)
-        if i == 0:
-            alpha = np.NaN
-        else:
-            lc_prev_row = lc.iloc[i-1]
-            date_1 = lc_prev_row['dateobs']
-            mag_1 = lc_prev_row['m']
+    alpha_temp['dateobs'] = lc['dateobs']
+    alpha_temp['filt'] = lc['filt']
 
-            if mag_1 == '-' or mag_2 == '-':
-                alpha = np.NaN
-            else:
-                alpha = (float(mag_2) - float(mag_1)) / (int(date_2) - int(date_1))
+    alpha_temp['mag_diff'] = lc['m'].diff()
+    alpha_temp['date_diff'] = lc['dateobs'].diff()
+    alpha_temp['alpha'] = alpha_temp['mag_diff'].values / alpha_temp['date_diff'].values
 
-            if verbose:
-                print(f'{filter} mag diff calculation:')
-                print(f'{mag_2} - {mag_1} / {date_2} - {date_1} = {alpha}')
-                print(' ')
-        
-        alpha_temp = alpha_temp.append([{'dateobs': date_2,
-                                         'filt':filter,
-                                         'alpha':alpha}])
     return alpha_temp
+
+
+    # for i in range(len(lc)):
+       
+    #     lc_row = lc.iloc[i]
+    #     date_2 = lc_row['dateobs']
+    #     filter = lc_row['filt']
+    #     mag_2 = lc_row['m']
+
+    #     # Calculate change in mag from previous detection, alpha (mag/night)
+    #     #### REPLACE W/ PD.DIFF + REPLACE '-' W/ PD.REPLACE() NANS
+    #     if i == 0:
+    #         alpha = np.NaN
+    #     else:
+    #         lc_prev_row = lc.iloc[i-1]
+    #         date_1 = lc_prev_row['dateobs']
+    #         mag_1 = lc_prev_row['m']
+
+    #         if mag_1 == '-' or mag_2 == '-':
+    #             alpha = np.NaN
+    #         else:
+    #             alpha = (float(mag_2) - float(mag_1)) / (int(date_2) - int(date_1))
+
+    #         if verbose:
+    #             print(f'{filter} mag diff calculation:')
+    #             print(f'{mag_2} - {mag_1} / {date_2} - {date_1} = {alpha}')
+    #             print(' ')
+        
+    #     alpha_temp = alpha_temp.append([{'dateobs': date_2,
+    #                                      'filt':filter,
+    #                                      'alpha':alpha}])
+    # return alpha_temp
 
 def mag_rates(lc_file, verbose=False):
 
