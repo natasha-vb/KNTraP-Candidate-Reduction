69ca9fdf06a383896893ad089b4d917f0374cb8d
natasha-vb <nvanbemmel@swin.edu.au>
2022-12-19 22:37:08 +1100

    Update mag_rates.py

--- utils/mag_rates.py
+++ utils/mag_rates.py
@@ -18,11 +18,11 @@ def calculate_mag_diff(lc, verbose=False):
     # print(lc['m'])
 
     alpha_temp['dateobs'] = lc['dateobs']
-    alpha_temp['dateobs'] = [int(x) for x in alpha_temp['dateobs']]
+    alpha_temp['dateobs_int'] = [int(x) for x in alpha_temp['dateobs']]
     alpha_temp['filt'] = lc['filt']
 
     alpha_temp['mag_diff'] = lc['m'].diff()
-    alpha_temp['date_diff'] = alpha_temp['dateobs'].diff()
+    alpha_temp['date_diff'] = alpha_temp['dateobs_int'].diff()
     alpha_temp['alpha'] = alpha_temp['mag_diff'].values / alpha_temp['date_diff'].values
 
     if verbose:
