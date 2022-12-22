d23014dc11f2b9101fa3b256ae8ded4a6eeaa17e
natasha-vb <nvanbemmel@swin.edu.au>
2022-12-23 03:04:01 +1100

    Update make_lightcurves.py

--- make_lightcurves.py
+++ make_lightcurves.py
@@ -117,8 +117,8 @@ if __name__ == "__main__":
             # ax.scatter(f_mi_cut['dateobs'].astype(float), f_mi_cut['m'].astype(float), edgecolors='r', facecolor=None, marker='o')
             # ax.scatter(f_mg_cut['dateobs'].astype(float), f_mg_cut['m'].astype(float), edgecolors='b', facecolor=None, marker='o')
 
-            ax.plot(f_mi_cut['dateobs'].astype(float), f_mi_cut['m'].astype(float), lw=0, c='r', marker='s')
-            ax.plot(f_mg_cut['dateobs'].astype(float), f_mg_cut['m'].astype(float), lw=0, c='b', marker='s')
+            ax.plot(f_mi_cut['dateobs'].astype(float), f_mi_cut['m'].astype(float), lw=0, c='r', marker='s', ms=2)
+            ax.plot(f_mg_cut['dateobs'].astype(float), f_mg_cut['m'].astype(float), lw=0, c='b', marker='s', ms=2)
             
             # Plot limiting magnitude
             ax.scatter(f_limi['dateobs'].astype(float), f_limi['limiting_mag'], c='r', marker='v', alpha=0.2)
@@ -129,8 +129,11 @@ if __name__ == "__main__":
             fillstyle='None', linestyle='None', ecolor='r')
             ax.errorbar(unf_mg['dateobs'].astype(float), unf_mg['m'].astype(float), yerr=unf_mg['dm'].astype(float), fmt='None', capsize=2,
             fillstyle='None', linestyle='None', ecolor='b')
-            ax.scatter(unf_mi['dateobs'].astype(float), unf_mi['m'].astype(float), c='r', marker='.', label='i band')
-            ax.scatter(unf_mg['dateobs'].astype(float), unf_mg['m'].astype(float), c='b', marker='.', label='g band')
+            # ax.scatter(unf_mi['dateobs'].astype(float), unf_mi['m'].astype(float), c='r', marker='.', label='i band')
+            # ax.scatter(unf_mg['dateobs'].astype(float), unf_mg['m'].astype(float), c='b', marker='.', label='g band')
+
+            ax.plot(unf_mi['dateobs'].astype(float), unf_mi['m'].astype(float), lw=0, c='r', marker='.', ms=2, label='i band')
+            ax.plot(unf_mg['dateobs'].astype(float), unf_mg['m'].astype(float), lw=0, c='b', marker='.', ms=2, label='g band')
 
             # Mark "good" detections
             ax.scatter(good_unf_i['dateobs'].astype(float), good_unf_i['m'].astype(float), c='r', marker='x')
