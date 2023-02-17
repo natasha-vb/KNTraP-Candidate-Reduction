2b15704b58a13311dc61fa09b363a642ff7adfb5
natasha-vb <nvanbemmel@swin.edu.au>
2023-02-17 14:39:24 +1100

    Update make_thumbnails.py

--- make_thumbnails.py
+++ make_thumbnails.py
@@ -3,6 +3,7 @@ import glob
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import pandas as pd
 
 import astropy.io.fits as fits
 from astropy.nddata import Cutout2D
@@ -12,45 +13,6 @@ from astropy.coordinates import SkyCoord
 from astropy.visualization import LinearStretch
 from astropy.visualization import ZScaleInterval
 from astropy.visualization import ImageNormalize
-
-
-## grab list of primary candidates from ./masterlists/field/priority/primary_candidates_{field}.csv ***might add secondary candidates as an option***
-## loop over candidates
-## use list to get ccd, ra and dec
-## grab fits images (template, science, difference) for all nights matching the candidate
-## make cutouts from coordinates of all images (only save 1 template image as these should all be the same --> or save all until after grid created, then delete all but first one)
-## save cutouts as .fits files (./lc_files/{field}/filtered_candidates/primary_candidates/thumbnails)
-## plot template, science and difference images in a grid, showing evolution over all nights
-## save evolution grid as .png
-
-# From Jielai Zhang
-def imshow_zscale(image,fits=False,grid=True,ticks=False):
-    if fits==True:
-        d = fits.getdata(image)
-    else:
-        d=image
-    norm = ImageNormalize(d, interval=ZScaleInterval(),stretch=LinearStretch())
-    plt.imshow(d, cmap=plt.cm.gray, norm=norm, interpolation='none')
-    if ticks:
-        plt.tick_params(labelsize=16)
-    if grid:
-        plt.grid()
-    return None
-
-
-def make_thumbnail_grid(cand_id, field, primary=False, secondary=False):
-    if primary:
-        cand_directory = 'primary_candidates'
-    if secondary:
-        cand_directory = 'secondary_candidates'
-    
-    # Grab i and g band cutouts 
-    i_thumbnails = glob.glob(f'./lc_files/{field}/filtered_candidates/{cand_directory}/thumbnails/cand{cand_id}*.i.*')
-    g_thumbnails = glob.glob(f'./lc_files/{field}/filtered_candidates/{cand_directory}/thumbnails/cand{cand_id}*.g.*')
-
-    fig, ax = plt.subplots(nrows=3, ncols=11)
-    for fits in i_thumbnails:
-        imshow_zscale(fits,grid=False,ticks=False)
 
 
 # From Jielai Zhang 
@@ -66,67 +28,156 @@ def create_cutout_centre(fitsfile,RA,DEC,image_size,verbose=False,debug=False):
     h.update(cutout.wcs.to_header())
     return cutout.data, h
 
+# From Jielai Zhang
+def make_stamps(RA,DEC,fitsfiles_2Darray,output='stamp.png',labels=False,size=50,debug=False):
+    '''fitsfiles_2Darray should be a 2D array that specifies the number of rows and columns in output stamps.
+    E.g. [[temp_1.fits,temp_2.fits],[sci_1.fits,sci2.fits],[sub_1.fits,sub_2.fits]] gives 3 rows and 2 columns.
+    '''
+            
+    ##################
+    # Set up subplot #
+    ##################
+    n_rows = np.shape(fitsfiles_2Darray)[0]
+    n_cols = np.shape(fitsfiles_2Darray)[1]   
+
+    if debug:
+        print(f"cutout shape: {np.shape(cutout)}")
+        
+    # start figure with pixel axis
+    fig, axs = plt.subplots(n_rows, n_cols,figsize=(n_cols*5,n_rows*5))
+    fig.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace=None)
+    
+    ##############################
+    # Plot one subplot at a time #
+    ##############################
+    for row in range(n_rows):
+        for col in range(n_cols):
+                
+            # Read in data, get cutout
+            cutout,cutout_h = create_cutout_centre(fitsfiles_2Darray[row][col],
+                                            RA,DEC,size,verbose=debug,debug=debug)
+            
+            # imshow, with pixel value normalisation
+            norm = ImageNormalize(cutout, interval=ZScaleInterval(),stretch=LinearStretch())
+            if n_rows == 1 or n_cols == 1:
+                axs[max(row,col)].imshow(cutout, norm = norm, cmap='gray') 
+            else:
+                axs[row, col].imshow(cutout, norm = norm, cmap='gray') 
+                
+            # labels
+            if col == 1 and row == 1:
+                axs[row, col].set_ylabel('Template', fontsize=30)
+            if col == 1 and row == 2:
+                axs[row,col].set_ylabel('Science', fontsize=30)
+            if col == 1 and row == 3:
+                axs[row,col].set_ylabel('Difference', fontsize=30)
+            
+            if labels:
+                labels = [[220213,220214,220215,220216,220217,220218,220219,220220,220221,220222],
+                          ['','','','','','','','','',''],
+                          ['','','','','','','','','','']]
+                label=labels[row][col]
+            else:
+                label=''
+
+            if n_rows == 1 or n_cols ==1:
+                axs[max(row,col)].set_axis_off()
+                axs[max(row,col)].set_title(label, fontsize=30)
+            else:
+                axs[row, col].set_axis_off()
+                axs[row, col].set_title(label, fontsize=30)
+                    
+    plt.savefig(output)
+    plt.close()
+
+    return output
+
+def make_thumbnail_grid(cand_id, ra, dec, field, primary=False, secondary=False):
+    if primary:
+        cand_directory = 'primary_candidates'
+    if secondary:
+        cand_directory = 'secondary_candidates'
+    
+    # Grab i and g band cutouts 
+    filters = ['i','g']
+    for filt in filters:
+        tmpl_thumbnails = glob.glob(f'./lc_files/{field}/filtered_candidates/{cand_directory}/thumbnails/cand{cand_id}*_{filt}*diff.tmpl.cutout.fits')
+        sci_thumbnails  = glob.glob(f'./lc_files/{field}/filtered_candidates/{cand_directory}/thumbnails/cand{cand_id}*_{filt}*diff.im.cutout.fits')
+        diff_thumbnails = glob.glob(f'./lc_files/{field}/filtered_candidates/{cand_directory}/thumbnails/cand{cand_id}*_{filt}*diff.cutout.fits')
+        thumbnail_array = [tmpl_thumbnails,sci_thumbnails,diff_thumbnails]
+        
+        output_name = f'cand{cand_id}_{filt}_thumbnail_grid.png'
+        print('output_name:', output_name)
+
+        make_stamps(ra,dec,thumbnail_array,output=output_name,labels=True)
+
+        print('Thumbnail grid for candidate {cand_id} saved!')
+        print('Save location:', output_name)
+
+   
 
 def create_cutout_files(cand_list, field, primary=False, secondary=False, verbose=False):
     # Iterate over masterlist to get candidate data
     for i in range(len(cand_list)):
-        cand    = cand_list.iloc[i]
-        ra      = cand['RA_AVERAGE']
-        dec     = cand['DEC_AVERAGE']
-        ccd     = cand['CCD']
-        cand_id = cand['CAND_ID']
+        cand_list_csv = pd.read_csv(m_pri[0],sep=',', comment='#', header=11, skipinitialspace=True)
+        for ii in range(len(cand_list_csv)):
+            cand    = cand_list.iloc[ii]
+            ra      = cand['RA_AVERAGE']
+            dec     = cand['DEC_AVERAGE']
+            ccd     = cand['CCD']
+            cand_id = cand['CAND_ID']
 
-        print('-------------------------------')
-        print(f'Creating thumbnails for candidate {cand_id}')
-        print(' ')
-        print(f'CCD: {ccd}')
-        print(f'RA: {ra}')
-        print(f'DEC: {dec}')
-        print(' ')
-        
-        sci_images  = glob.glob(f'../../workspace/{field}_tmpl/{ccd}/*.diff.im.fits')
-        diff_images = glob.glob(f'../../workspace/{field}_tmpl/{ccd}/*.diff.fits')
-        tmpl_images = glob.glob(f'../../workspace/{field}_tmpl/{ccd}/*.diff.tmpl.fits')
-
-        if verbose:
-            print('Images found:')
-            print(sci_images)
+            print('--------------------------------------')
+            print(f'Creating thumbnails for candidate {cand_id}')
             print(' ')
-            print(diff_images)
+            print(f'CCD: {ccd}')
+            print(f'RA: {ra}')
+            print(f'DEC: {dec}')
             print(' ')
-            print(tmpl_images)
-            print(' ')
+            
+            sci_images  = glob.glob(f'../../workspace/{field}_tmpl/{ccd}/*.diff.im.fits')
+            diff_images = glob.glob(f'../../workspace/{field}_tmpl/{ccd}/*.diff.fits')
+            tmpl_images = glob.glob(f'../../workspace/{field}_tmpl/{ccd}/*.diff.tmpl.fits')
 
-        # Looping over single night images for science, difference, and template images
-        images_list = [sci_images, diff_images, tmpl_images]
-        for images in images_list:
-            for fits in images:
-                # Make cutout of candidate
-                data, header = create_cutout_centre(fits, ra, dec, 100) # look into image size???
+            if verbose:
+                print('Images found:')
+                print(sci_images)
+                print(' ')
+                print(diff_images)
+                print(' ')
+                print(tmpl_images)
+                print(' ')
 
-                # Save cutout as .fits to appropriate path
-                fits_name = fits.split('/')[-1]
-                fits_name = fits.replace(f'.fits', '.cutout.fits')
-                candfits_name = 'cand' + cand_id.astype(str) + '_' + fits_name
+            # Looping over single night images for science, difference, and template images
+            images_list = [sci_images, diff_images, tmpl_images]
+            for images in images_list:
+                for fits in images:
+                    # Make cutout of candidate
+                    data, header = create_cutout_centre(fits, ra, dec, 100) # look into image size???
 
-                print(f'Image thumbnail filename: {candfits_name}')
+                    # Save cutout as .fits to appropriate path
+                    fits_name = fits.split('/')[-1]
+                    fits_name = fits.replace(f'.fits', '.cutout.fits')
+                    candfits_name = 'cand' + cand_id.astype(str) + '_' + fits_name
 
-                if primary:
-                    cand_directory = 'primary_candidates'
-                if secondary:
-                    cand_directory = 'secondary_candidates'
+                    print(f'Image thumbnail filename: {candfits_name}')
 
-                # Create directory for thumbnail if not already existing
-                thumbnail_outdir = (f'./lc_files/{field}/filtered_candidates/{cand_directory}/thumbnails')
-                if not os.path.exists(thumbnail_outdir):
-                    os.makedirs(thumbnail_outdir)
+                    if primary:
+                        cand_directory = 'primary_candidates'
+                    if secondary:
+                        cand_directory = 'secondary_candidates'
 
-                fits.writeto(f'{thumbnail_outdir}/{candfits_name}', data, header=header, overwrite=True)
+                    # Create directory for thumbnail if not already existing
+                    thumbnail_outdir = (f'./lc_files/{field}/filtered_candidates/{cand_directory}/thumbnails')
+                    if not os.path.exists(thumbnail_outdir):
+                        os.makedirs(thumbnail_outdir)
 
-                print(f'Thumbnail saved to: {thumbnail_outdir}')
+                    fits.writeto(f'{thumbnail_outdir}/{candfits_name}', data, header=header, overwrite=True)
 
-        # From saved thumbnails, create .png evolution grid of images
-        _ = make_thumbnail_grid(cand_id, field=field, primary=primary, secondary=secondary)
+                    print(f'Thumbnail saved to: {thumbnail_outdir}')
+
+            # From saved thumbnails, create .png evolution grid of images
+            _ = make_thumbnail_grid(cand_id, ra, dec, field=field, primary=primary, secondary=secondary)
 
 
 if __name__ == "__main__":
@@ -168,4 +219,3 @@ if __name__ == "__main__":
         print('Found masterlist: ', m_sec)
 
         _ = create_cutout_files(m_sec, args.field, secondary=True, verbose=args.verbose)
-
